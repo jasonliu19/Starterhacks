6b7457aa12cf0edfb8e6974a25ff7e466940bf61
Jason L <jasonliu1999@gmail.com>
2018-03-03 22:41:20 -0500

    Added enemy spawning

---
+++ Assets/Enemy/spawn.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour {
+
+    // we can spawn multiple enemies in the array
+    public GameObject enemy;
+    public Vector3 spawnValues;
+    public float spawnWait;
+    public float spawnMostWait;
+    public float spawnLeastWait;
+    public int startWait;
+    public bool stop;
+    public float ypos;
+
+    int randEnemy;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(waitSpawner());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+	}
+  
+
+    IEnumerator waitSpawner()
+    {
+        yield return new WaitForSeconds(startWait); // how long before it starts spawning objects
+
+        while (!stop)
+        {
+            float xpos = Random.Range(-spawnValues.x, spawnValues.x);
+            float ypos = Random.Range(-spawnValues.y, spawnValues.y);
+
+            // x, y, z: x is between neg to pos spawnvalues, y = 1, z is similar x
+            Vector3 spawnPosition = new Vector3 (xpos,1,ypos);
+
+            Instantiate(enemy, spawnPosition + transform.TransformPoint(0, 0, 0), Quaternion.LookRotation(Vector3.zero));
+
+            yield return new WaitForSeconds(spawnWait);
+        }
+
+    }
+}
--- assets/Enemy/enemyMovement.cs
+++ assets/Enemy/enemyMovement.cs
@@ -5,8 +5,22 @@ using UnityEngine;
 public class enemyMovement : MonoBehaviour
 {
     // Update is called once per frame
-    void Update()
+    public Rigidbody rb;
+    public int speed;
+    Vector3 thisVelocity;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        thisVelocity = (-1.0F*rb.position.normalized)*speed;
+    }
+    void FixedUpdate()
+    {
+        rb.velocity = thisVelocity;
+        rb.rotation = Quaternion.identity;
+
+    }
+    /*void Update()
     {
         transform.Translate(2 * Vector3.forward * Time.deltaTime);
-    }
+    }*/
 }
