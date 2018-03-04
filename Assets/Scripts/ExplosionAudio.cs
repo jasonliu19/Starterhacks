4508af93860fd7ca8084fec15a8f78e4d35e82fd
Bruce Hong <bjhong@yahoo.com>
2018-03-04 13:36:34 -0500

    With grenade audio

---
+++ Assets/Scripts/ExplosionAudio.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionAudio : MonoBehaviour {
+
+    public AudioClip grenadeAudio;
+    public AudioSource grenadeSource;
+
+	// Use this for initialization
+	void Start () {
+        grenadeSource.clip = grenadeAudio;
+	}
+
+
+}
