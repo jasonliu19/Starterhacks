f5157786d54337220bc4456e3e6e378fdecb8fde
Jason L <jasonliu1999@gmail.com>
2018-03-03 22:04:48 -0500

    Cleaning build and added new map

---
+++ Assets/GestureHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestureHandler : MonoBehaviour {
+
+	public void PrintActivate()
+    {
+        print("Activate");
+    }
+
+    public void PrintDeactivate()
+    {
+        print("Deactivate");
+    }
+}
---
+++ Assets/GestureHandlerLeft.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestureHandlerLeft : MonoBehaviour {
+
+    // Use this for initialization
+    public Rigidbody grenade;
+
+	public void CreateGrenade()
+    {
+        Instantiate(grenade, new Vector3(0, 2, 0), Quaternion.identity);
+    }
+}
